7eff3643d32522396e8bbb4dae3c350069c97480
Federico Matías <41979395+FEDERICOMB96@users.noreply.github.com>
2025-04-27 12:03:07 -0300

    Implementación algoritmo BFS

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -23,5 +23,34 @@ class BreadthFirstSearch:
         
         # Add the node to the explored dictionary
         explored[node.state] = True
+
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
         
-        return NoSolution(explored)
+        frontier = QueueFrontier()
+        frontier.add(node)
+
+        while True:
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.remove()
+            successors = grid.get_neighbours(node.state) #{a1:s1, a2:s2,...}
+            acciones = successors.keys()
+            
+            for action in acciones:
+                new_state = successors[action]
+
+                if new_state not in explored:
+                    new_node = Node("", 
+                                    new_state,
+                                    node.cost + grid.get_cost(new_state),
+                                    node,
+                                    action)
+                    
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+                    
+                    explored[new_state] = True
+                    frontier.add(new_node)

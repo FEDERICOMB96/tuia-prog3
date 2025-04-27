eab9b5ec347d301045d0d147d7f338130c7c134c
Federico Matías <41979395+FEDERICOMB96@users.noreply.github.com>
2025-04-27 12:36:23 -0300

    Implementación algoritmo UCS

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -21,7 +21,33 @@ class UniformCostSearch:
         # Initialize the explored dictionary to be empty
         explored = {} 
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost)
+        explored[node.state] = node.cost
+
+        while True:
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.pop()       
+
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            successors = grid.get_neighbours(node.state) #{a1:s1, a2:s2,...}
+            acciones = successors.keys()
+            
+            for action in acciones:
+                new_state = successors[action]
+
+                cost = node.cost + grid.get_cost(new_state)
+
+                if new_state not in explored or cost < explored[new_state]:
+                    new_node = Node("", 
+                                    new_state,
+                                    cost,
+                                    node,
+                                    action)
+                    
+                    explored[new_state] = cost
+                    frontier.add(new_node, new_node.cost)

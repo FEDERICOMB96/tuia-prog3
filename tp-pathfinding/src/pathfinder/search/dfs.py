aba45c8dd5769dda084ca290233fab5d11819b26
Federico Matías <41979395+FEDERICOMB96@users.noreply.github.com>
2025-04-27 12:33:56 -0300

    Implementación algoritmo DFS

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -21,7 +21,43 @@ class DepthFirstSearch:
         # Initialize the explored dictionary to be empty
         explored = {} 
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
         
-        return NoSolution(explored)
+        frontier = StackFrontier()
+        frontier.add(node)
+
+        while True:
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.remove()
+
+            if node.state in explored:
+                continue
+
+            explored[node.state] = True
+
+            successors = grid.get_neighbours(node.state) #{a1:s1, a2:s2,...}
+            acciones = successors.keys()
+            
+            for action in acciones:
+                new_state = successors[action]
+
+                if new_state not in explored:
+                    new_node = Node("", 
+                                    new_state,
+                                    node.cost + grid.get_cost(new_state),
+                                    node,
+                                    action)
+                    
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+                    
+                    frontier.add(new_node)
+    
+
+
+
+
